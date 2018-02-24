be88acaa4c3fd41204987c9ae2f0c1f3972ef378
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-02-24 23:20:10 +0000

    Validate user model and post model. Display posts on root page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.paginate(page: params[:page])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: {in: 5..50}
+  validates :body, presence: true, length: {minimum: 10}
+  validates :user_id, presence: true
 end
